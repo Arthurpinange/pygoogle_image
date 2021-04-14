3b3c7490bc04abdd26210419bf8a14083f8360b5
Arthur Pinangé <arthur.pinange@gmail.com>
2021-04-13 23:25:56 -0300

    configuração do setup para empacotamento

--- pygoogle_image/__init__.py
+++ pygoogle_image/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import
 
-__version__ = '1.1'
+__version__ = '1.0.0'
 __author__ = 'Arthur Pinangé'
 __email__ = 'arthur.pinange@email.com'
--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
 
+_long_description = ''
+with open("README.md", "r", encoding="utf-8") as fh:
+    _long_description = fh.read()
 
+print(open('README.md').read())
 setup_args = dict(name='pygoogle_image',
-      version='1.1',
+      version='1.0.0',
       description='the package downloads images from google images',
+      long_description=_long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/Arthurpinange/pygoogle_image',
       author='Arthur Pinangé',
       author_email='arthur.pinange@gmail.com',
